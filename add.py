3851f21da7359a38370c2506532427a0e534949d
Dhilip <dhilipvishnu925@gmail>
2023-09-17 03:28:40 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, jsonify
+from transformers import pipeline
+import json
+import datetime
+
+app = Flask(__name__)
+
+# Load your QA pairs here
+def load_qa_pairs(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print(f"An error occurred while loading data: {e}")
+        return {}
+
+qa_pairs = load_qa_pairs("qa_pairs.json")
+
+# Load the chatbot model
+bot = pipeline("conversational")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/process_message", methods=["POST"])
+def process_message():
+    user_message = request.json.get("message", "")
+    response = get_response(user_message, qa_pairs)
+    if not response:
+        response = bot(user_message)[0]['message']['content']
+    return jsonify({"message": response})
+
+@app.route("/submit_feedback", methods=["POST"])
+def submit_feedback():
+    feedback_data = request.json
+    sentiment = feedback_data.get("sentiment", "")
+    comment = feedback_data.get("comment", "")
+    
+    if sentiment and comment:
+        # Store feedback data in a JSON file with a timestamp
+        timestamp = datetime.datetime.now().isoformat()
+        feedback_entry = {"timestamp": timestamp, "sentiment": sentiment, "comment": comment}
+        with open("feedback.json", "a", encoding="utf-8") as feedback_file:
+            json.dump(feedback_entry, feedback_file, ensure_ascii=False)
+            feedback_file.write("\n")
+        
+        # Perform basic analysis here (e.g., count positive/negative feedback)
+
+        # Return a simple acknowledgment
+        return jsonify({"status": "Feedback received"})
+
+    return jsonify({"error": "Invalid feedback data"}), 400
+
+def get_response(user_input, qa_pairs):
+    user_input = user_input.lower()
+    for section, pairs in qa_pairs.items():
+        for pair in pairs:
+            if user_input in pair["question"].lower():
+                return pair["answer"]
+    return "I'm sorry, I don't have the answer to that question."
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ bot.py
@@ -0,0 +1,45 @@
+import json
+from transformers import pipeline
+
+# Load the chatbot model
+bot = pipeline("conversational")
+
+def load_qa_pairs(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print(f"An error occurred while loading data: {e}")
+        return {}
+
+def get_response(user_input, qa_pairs):
+    user_input = user_input.lower()
+    for section, pairs in qa_pairs.items():
+        for pair in pairs:
+            if user_input in pair["question"].lower():
+                return pair["answer"]
+    return "I'm sorry, I don't have the answer to that question."
+
+def main():
+    qa_filename = "qa_pairs.json"  # Replace with the path to your JSON file
+    qa_pairs = load_qa_pairs(qa_filename)
+
+    print("Chatbot: Hello! How can I assist you today? (Type 'exit' to end the conversation)")
+
+    while True:
+        user_input = input("You: ")
+        
+        if user_input.lower() in ['exit', 'quit', 'stop']:
+            print("Chatbot: Goodbye!")
+            break
+        
+        response = get_response(user_input, qa_pairs)
+        if response:
+            print("Chatbot:", response)
+        else:
+            response = bot(user_input)
+            print("Chatbot:", response[0]['message']['content'])
+
+if __name__ == "__main__":
+    main()
